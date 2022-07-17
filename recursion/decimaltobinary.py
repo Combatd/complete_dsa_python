5bf3a1064e26a701563c2d506cebc2c7d66d1443
Combatd <combatdstar@gmail.com>
2022-07-17 15:39:02 -0700

    Define #decimalToBinary

--- recursion/decimaltobinary.py
+++ recursion/decimaltobinary.py
@@ -1,2 +1,13 @@
 def decimalToBinary(n):
-    return n
+    # unintentional case
+    assert int(n) == n, 'The number must be an integer only'
+    # base case
+    if n == 0:
+        return 0
+    else:
+    # recursive case : f(n) = n % 2 + 10 * f(n/2)
+        return n % 2 + 10 * decimalToBinary(int(n / 2))
+
+print(decimalToBinary(10))
+print(decimalToBinary(13))
+print(decimalToBinary(-13))
