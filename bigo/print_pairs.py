ca7b567eb64473b48f10b5bbe08ebb208a39cba7
Combatd <combatdstar@gmail.com>
2022-07-18 20:01:11 -0700

    Comment #printPairs

--- bigo/print_pairs.py
+++ bigo/print_pairs.py
@@ -1,4 +1,6 @@
 def printPairs(array):
-    for i in array:
-        for i in array:
-            print(str(i) + "," + str(j))
+    for i in array: # O(n)
+        for i in array: # O(n) at every iteration
+            print(str(i) + "," + str(j)) # O(1)
+
+# In a for loop of O(n), each n iteration has an O(n) for loop that runs. n(n) = O(n^2) quadratic
