4c8c0417e0133fb6af8239056dcdc0cb97a6847f
Combatd <combatdstar@gmail.com>
2022-07-18 19:52:15 -0700

    Define #productAndSum

---
+++ bigo/product_and_sum.py
@@ -0,0 +1,11 @@
+def productAndSum(array):
+    sum = 0
+    product = 1
+
+    for i in array:
+        sum += 1
+
+    for i in array:
+        product += 1
+
+    print("Sum = " + str(sum) + " Product + " + str(product))
