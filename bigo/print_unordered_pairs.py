3df628107dd318a8db742f4add999f7235d4cbc2
Combatd <combatdstar@gmail.com>
2022-07-18 21:25:13 -0700

    Comment #printUnorderedPairs

--- bigo/print_unordered_pairs.py
+++ bigo/print_unordered_pairs.py
@@ -1,5 +1,6 @@
 def printUnorderedPairs(array):
-    for i in range(0, len(array)): # O(n) with i index
-        for j in range(i + 1, len(array)): # O(n - 1) with i index
+    for i in range(0, len(array)): # O(n-1) -> O(n) with i index
+        for j in range(i + 1, len(array)): # O(n-2) -> O(n) with j index
             print(array[i], + "," + array[j]) # O(1)
 
+# It has O(n) * O(n - 1) = O(n^2)
