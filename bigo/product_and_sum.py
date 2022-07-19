9aa9009d2262168bd98b40c0b7d8d9dfa1635e50
Combatd <combatdstar@gmail.com>
2022-07-18 19:55:42 -0700

    Comment on productAndSum

--- bigo/product_and_sum.py
+++ bigo/product_and_sum.py
@@ -1,11 +1,13 @@
 def productAndSum(array):
-    sum = 0
-    product = 1
+    sum = 0 # O(1)
+    product = 1 O(1)
 
-    for i in array:
-        sum += 1
+    for i in array: # O(n)
+        sum += 1 # O(1)
 
-    for i in array:
-        product += 1
+    for i in array: # O(n)
+        product += 1 # O(1)
 
-    print("Sum = " + str(sum) + " Product + " + str(product))
+    print("Sum = " + str(sum) + " Product + " + str(product)) # O(1)
+
+    # Has 2 O(n) "for loops" which is O(2n) = O(n)
