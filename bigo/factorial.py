2b9297b5ac27c5918bb6da5fd07a8bb70e73c27f
Combatd <combatdstar@gmail.com>
2022-07-21 18:28:47 -0700

    Comment #factorial

--- bigo/factorial.py
+++ bigo/factorial.py
@@ -1,7 +1,18 @@
-def factorial(n):
+def factorial(n): # M(n)
     if n < 0:
-        return -1
+        return -1 # O(1)
     elif n == 0:
-        return 1
+        return 1 # O(1)
     else:
-        return n * factorial(n-1)
+        return n * factorial(n-1) # M(n - 1)
+
+    # We need to measure the recursive algorithm here
+    # n! = 1 * 2 * 3..... * n
+    #  3! = 1 * 2 * 3 = 6
+    # M(n-1) = O(1) + M((n-1))-1  
+    # M(n-2) = O(1) + M((n-2))-1
+    # a + M(n-a)
+    # n + M(n - n)  
+    # n + 1
+    # n
+    # The time complexity is O(n)
