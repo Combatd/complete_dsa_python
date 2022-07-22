18cbbe22a77ebe1cde83cc788f19de981cd4ed40
Combatd <combatdstar@gmail.com>
2022-07-22 03:48:52 -0700

    Define #accessElement

--- arrays/createArray.py
+++ arrays/createArray.py
@@ -10,7 +10,15 @@ arr2 = array('d', [1.3, 1.5, 1.6])
 # print(arr1)
 
 def traverseArray(array): # Space Complexity = O(1)
-    for i in array: # O(n)
-        print(i) # O(1)
+    for i in array: # O(n) Time
+        print(i) # O(1) Time
 
 traverseArray(arr1)
+
+def accessElement(array, index): # Space Complexity: O1()
+    if index >= len(array): # negative indexes for access from end are supported in python
+        print("There is not any element in this index") # O(1) Time
+    else:
+        print(array[index]) # O(1) Time
+
+accessElement(arr1, 6)
