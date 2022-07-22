b03963ce0b5f522326c08ab23e1e017002287dac
Combatd <combatdstar@gmail.com>
2022-07-22 03:34:16 -0700

    Define #traverseArray

--- arrays/createArray.py
+++ arrays/createArray.py
@@ -3,8 +3,14 @@ from array import *
 arr1 = array('i', [1,2,3,4,5,6])
 arr2 = array('d', [1.3, 1.5, 1.6])
 
-print(arr1)
-print(arr2)
+# print(arr1)
+# print(arr2)
 
-arr1.insert(0, 0) # O(n) worst case
-print(arr1)
+# arr1.insert(0, 0) # O(n) worst case
+# print(arr1)
+
+def traverseArray(array): # Space Complexity = O(1)
+    for i in array: # O(n)
+        print(i) # O(1)
+
+traverseArray(arr1)
