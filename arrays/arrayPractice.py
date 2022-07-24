dab8304d244f580f2a8b25552e9dfce6151d7a47
Combatd <combatdstar@gmail.com>
2022-07-24 08:24:34 -0700

    Traverse array

---
+++ arrays/arrayPractice.py
@@ -0,0 +1,9 @@
+from array import *
+
+# 1. Create an array and traverse
+
+my_array = array('i', [1, 2, 3, 4, 5])
+
+for i in my_array:
+    print(i, ' <- traverse and printing out elements one by one')
+
