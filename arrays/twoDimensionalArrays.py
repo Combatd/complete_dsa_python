4d337694528bcb2adb39d5bb04d562abd5d4beec
Combatd <combatdstar@gmail.com>
2022-07-25 12:44:39 -0700

    Traverse array

--- arrays/twoDimensionalArrays.py
+++ arrays/twoDimensionalArrays.py
@@ -13,6 +13,13 @@ def accessElements(array, rowIndex, colIndex):
     if rowIndex >= len(array) and colIndex >= len(array[0]): # O(1)
         print("Incorrect index") # O(1)
     else:
-        print(array[rowIndex][colIndex]) #O(1)
+        print(array[rowIndex][colIndex]) # O(1)
 
 accessElements(twoDArray, 2, 3)
+
+def traverse2DArray(array): # Space Complexity: O(1) Time Complexity: O(n^2)
+    for i in range(len(array)): # O(mn) as it is rows and columns
+        for j in range(len(array[0])): # 0(n) to access columns
+            print(array[i][j]) # O(1)
+
+traverse2DArray(twoDArray)
