eee3ffed6d361eb0fec01a8dce992c35bf97474b
Combatd <combatdstar@gmail.com>
2022-07-25 13:55:57 -0700

    Test out lists

---
+++ lists/createList.py
@@ -0,0 +1,11 @@
+integers = [1, 2, 3, 4]
+print(integers)
+
+stringList = ['Milk', 'Cheese', 'Butter']
+print(stringList)
+
+mixList = [1, 1.5, 'spam']
+print(mixList)
+
+nestedList = [1, 2, 4, 5, [1.5, 1.6], ['test']]
+print(nestedList)
