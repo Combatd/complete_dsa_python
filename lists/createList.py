f9f1334b9825624a57dec97e3c8881fd13f29bf5
Combatd <combatdstar@gmail.com>
2022-07-25 14:04:41 -0700

    Access and traverse a list

--- lists/createList.py
+++ lists/createList.py
@@ -9,3 +9,19 @@ print(mixList)
 
 nestedList = [1, 2, 4, 5, [1.5, 1.6], ['test']]
 print(nestedList)
+
+# Accessing/Traversing the List
+
+shoppingList = ['Milk', 'Cheese', 'Butter'] 
+print(shoppingList[0]) # access the first element
+print(shoppingList[-1]) # last element
+# Mapping, each index maps to one of the elements
+
+print('Milk' in shoppingList)
+
+for i in shoppingList:
+    print(i)
+
+for idx in range(len(shoppingList)):
+    shoppingList[idx] = shoppingList[idx] + '+'
+    print(shoppingList[idx])
