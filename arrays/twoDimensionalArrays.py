547b579bb4486586214a3f6216202444eee5538a
Combatd <combatdstar@gmail.com>
2022-07-25 12:27:30 -0700

    Comment #accessElements

--- arrays/twoDimensionalArrays.py
+++ arrays/twoDimensionalArrays.py
@@ -10,9 +10,9 @@ print(twoDArray)
 # print(newTwoDArray)
 
 def accessElements(array, rowIndex, colIndex):
-    if rowIndex >= len(array) and colIndex >= len(array[0]):
-        print("Incorrect index")
+    if rowIndex >= len(array) and colIndex >= len(array[0]): # O(1)
+        print("Incorrect index") # O(1)
     else:
-        print(array[rowIndex][colIndex])
+        print(array[rowIndex][colIndex]) #O(1)
 
 accessElements(twoDArray, 2, 3)
