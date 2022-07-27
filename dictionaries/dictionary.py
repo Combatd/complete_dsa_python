e70aea82d71c76cfbf207e4f152ed3bbe578f3e0
Combatd <combatdstar@gmail.com>
2022-07-27 09:19:39 -0700

    Insert element into dictionary

--- dictionaries/dictionary.py
+++ dictionaries/dictionary.py
@@ -4,7 +4,12 @@
 # secondDict = {}
 # print(secondDict)
 
-myDict = {1: 'one', 2: 'two'}
-engToSp = {'one': 'uno', 'two': 'dos', 'three': 'tres'}
-print(engToSp)
-print(engToSp['one']) # Time Complexity: O(1)
+# myDict = {1: 'one', 2: 'two'}
+# engToSp = {'one': 'uno', 'two': 'dos', 'three': 'tres'}
+# print(engToSp)
+# print(engToSp['one']) # Time Complexity: O(1)
+
+myDict = {'name': 'Eddy', 'age': 26}
+myDict['age'] = 27 # Time Complexity: O(1) Space Complexity: O(1)
+myDict['address'] = 'London' # Time Complexity: O(1) Space Complexity: amortized O(1) - underlying linked list needs to grow
+print(myDict)
