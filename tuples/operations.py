9ce606cb2775cbb3872d15ad000ed844d56272f1
Combatd <combatdstar@gmail.com>
2022-07-27 11:10:24 -0700

    Traerse tuple

--- tuples/operations.py
+++ tuples/operations.py
@@ -4,3 +4,10 @@ print(newTuple[1])
 print(newTuple[-1])
 
 print(newTuple[1:])
+
+# Traverse Tuple
+for i in newTuple: # Time: O(n) Space: O(1)
+    print(i) # O(1)
+
+for i in range(len(newTuple)):
+    print(newTuple[i])
