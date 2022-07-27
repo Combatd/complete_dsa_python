d091b22b56ce434800fa9e08439d8713a263bd89
Combatd <combatdstar@gmail.com>
2022-07-26 17:43:52 -0700

    Slice and delete elements from list

--- lists/listOperations.py
+++ lists/listOperations.py
@@ -13,6 +13,21 @@ print(myList)
 # myList.append(55) # Time Complexity: O(1)
 # print(myList)
 
-newList = [11, 12, 13, 14]
-myList.extend(newList) # Time Complexity: O(n) Space Complexity: O(n)
+# newList = [11, 12, 13, 14]
+# myList.extend(newList) # Time Complexity: O(n) Space Complexity: O(n)
+# print(myList)
+
+myList = ['a', 'b', 'c', 'd', 'e', 'f']
+
+# myList[0:2] = ['x', 'y']
+# print(myList[:]) # slice is exclusive of end index
+
+# myList.pop(2) # second element in the list - Time Complexity: O(n)
+# print(myList.pop()) # last element in the list - Time Complexity: O(1)
+# print(myList)
+
+# del myList[2:4] # Time Complexity O(n)
+# print(myList)
+
+myList.remove('e') # Time Complexity: O(n)
 print(myList)
