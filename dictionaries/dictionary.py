a57205decbd5e18a5c0db93bf7c5fbe714c14fae
Combatd <combatdstar@gmail.com>
2022-07-27 09:39:19 -0700

    Define #traverseDict"

--- dictionaries/dictionary.py
+++ dictionaries/dictionary.py
@@ -13,3 +13,9 @@ myDict = {'name': 'Eddy', 'age': 26}
 myDict['age'] = 27 # Time Complexity: O(1) Space Complexity: O(1)
 myDict['address'] = 'London' # Time Complexity: O(1) Space Complexity: amortized O(1) - underlying linked list needs to grow
 print(myDict)
+
+def traverseDict(dict): # Time Complexity: O(n), Space Compleixty: O(1)
+    for key in dict: # O(n)
+        print(key, dict[key]) # O(1)
+
+traverseDict(myDict)
