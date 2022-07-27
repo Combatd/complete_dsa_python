b951f2d5eea663e010aa7161c04df8a061fdbefd
Combatd <combatdstar@gmail.com>
2022-07-27 11:14:54 -0700

    Define #searchTuple

--- tuples/operations.py
+++ tuples/operations.py
@@ -11,3 +11,17 @@ for i in newTuple: # Time: O(n) Space: O(1)
 
 for i in range(len(newTuple)):
     print(newTuple[i])
+
+print('b' in newTuple) # True
+print('f' in newTuple) # False
+
+# Linear Search
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def searchTuple(pTuple, element):
+    for i in pTuple: # O(n)
+        if i == element: # O(1)
+            return pTuple.index(i) # you can also return i, O(1)
+    return 'The element does not exist'
+
+print(searchTuple(newTuple, 'c'))
