3b200679a23359f7ca336b3e8c23e857c7385d3d
Combatd <combatdstar@gmail.com>
2022-07-27 10:55:00 -0700

    Declare tuples

---
+++ tuples/tuple.py
@@ -0,0 +1,9 @@
+# Time Complexity: O(1)
+# Space Complexity: O(n)
+newTuple = ('a', 'b', 'c', 'd', 'e')
+newTuple1 = ('a',) # if 1 element tuple does not have end comma, it becomes string
+newTuple2 = tuple()
+
+print(newTuple)
+print(newTuple1)
+print(newTuple2)
