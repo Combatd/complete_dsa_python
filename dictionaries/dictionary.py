b982d31dab2eb212a35e0caa0012ecb68ee7186c
Combatd <combatdstar@gmail.com>
2022-07-27 08:49:29 -0700

    Declare dictionaries

---
+++ dictionaries/dictionary.py
@@ -0,0 +1,10 @@
+# myDict = dict()
+# print(myDict)
+
+# secondDict = {}
+# print(secondDict)
+
+myDict = {1: 'one', 2: 'two'}
+engToSp = {'one': 'uno', 'two': 'dos', 'three': 'tres'}
+print(engToSp)
+print(engToSp['one']) # Time Complexity: O(1)
