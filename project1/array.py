8f7a9a3418a4a5124dd380cd9c945e35e3573c5b
Combatd <combatdstar@gmail.com>
2022-07-27 05:53:09 -0700

    Count days above average temperature

--- project1/array.py
+++ project1/array.py
@@ -1,9 +1,18 @@
 numDays = int(input("How many days' temperature? "))
 total = 0
+temp = []
 
-for i in range(1, numDays + 1):
-    nextDay = int(input("Day " + str(i) + "'s high temp:"))
-    total += nextDay
+for i in range(numDays):
+    nextDay = int(input("Day " + str(i + 1) + "'s high temp:"))
+    temp.append(nextDay)
+    total += temp[i]
 
 avg = round(total / numDays, 2)
 print("\nAverage = " + str(avg))
+
+above = 0
+for i in temp:
+    if i > avg:
+        above += 1
+
+print(str(above) + " days(s) above average")
