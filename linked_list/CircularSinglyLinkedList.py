e58163308ed759d62eb73116dbcc38496504cebc
Combatd <combatdstar@gmail.com>
2022-07-30 20:50:54 -0700

    define #insertCSLL

--- linked_list/CircularSinglyLinkedList.py
+++ linked_list/CircularSinglyLinkedList.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, value): # Time: O(1)
+    def __init__(self, value = None): # Time: O(1)
         self.value = value
         self.next = None
 
@@ -12,9 +12,10 @@ class CircularSinglyLinkedList:
         node = self.head
         while node:
             yield node
-            if node.next == self.head:
-                break
             node = node.next
+            if node.next == self.tail.next:
+                break
+            
 
     # Creation of Circular Singly Linked List
     # Time Complexity: O(1) Space Complexity: O(1)
@@ -25,7 +26,40 @@ class CircularSinglyLinkedList:
         self.tail = node # O(1)
         return "The Circular Singly Linked List has been created" # O(1)
 
+    # Insertion of a node in Circular Singly Linked List
+    # Time Complexity: O(n) Space Complexity: O(1)
+    def insertCSLL(self, value, location):
+        if self.head is None:
+            return "The head reference is None"
+        else:
+            newNode = Node(value)
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+                self.tail.next = newNode # Circles around
+            elif location == 1:
+                newNode.next = self.tail.next
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1: # O(n)
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+            return "The node has been successfully inserted"
+
 circularSLL = CircularSinglyLinkedList()
-circularSLL.createCSLL(1)
+print(circularSLL.createCSLL(1))
+
+print(circularSLL.insertCSLL(0, 0)) # insert at head
+print(circularSLL.insertCSLL(2, 1)) # insert at tail
+print(circularSLL.insertCSLL(3, 1))
+print(circularSLL.insertCSLL(2, 2))
+
+
 
 print([node.value for node in circularSLL])
