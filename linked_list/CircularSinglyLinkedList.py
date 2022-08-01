33b842beb0915629ffe8bfa61448fe4f360a0644
Combatd <combatdstar@gmail.com>
2022-07-31 17:41:10 -0700

    Define #deleteNode

--- linked_list/CircularSinglyLinkedList.py
+++ linked_list/CircularSinglyLinkedList.py
@@ -78,19 +78,56 @@ class CircularSinglyLinkedList:
                 tempNode = tempNode.next
                 if tempNode == self.tail.next:
                     return "The node does not exist in this Circular Singly Linked List"
-        
-
+    
+    # Delete a node from Circularl Singly Linked List
+    # Time Complexity: O(n) Space Complexity: O(1)
+    def deleteNode(self, location):
+        if self.head is None: # O(1)
+            print("There is not any node in the Circular Singly Linked List")
+        else:
+            if location == 0: # O(1)
+                if self.head == self.tail: # O(1)
+                    self.head.next = None
+                    self.head = None
+                    self.tail = None
+                else: # O(1)
+                    self.head = self.head.next
+                    self.tail.next = self.head
+            elif location == 1: # O(1)
+                if self.head == self.tail: # O(1)
+                    self.head.next = None
+                    self.head = None
+                    self.tail = None
+                else: # O(1)
+                    node = self.head
+                    while node is not None: # O(n)
+                        if node.next == self.tail: # O(n)
+                            break
+                        node = node.next
+                    node.next = self.head
+                    self.tail = node
+            else: # O(1)
+                tempNode = self.head
+                index = 0
+                while index < location - 1: # O(n)
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = nextNode.next
 
 
 circularSLL = CircularSinglyLinkedList()
-print(circularSLL.createCSLL(1))
+print(circularSLL.createCSLL(0))
 
 print(circularSLL.insertCSLL(0, 0)) # insert at head
 print(circularSLL.insertCSLL(2, 1)) # insert at tail
 print(circularSLL.insertCSLL(3, 1))
-print(circularSLL.insertCSLL(2, 2))
+print(circularSLL.insertCSLL(4, 1))
 
 print(circularSLL.traversalCSLL())
 print(circularSLL.searchCSLL(3))
 
+print([node.value for node in circularSLL])
+
+circularSLL.deleteNode(0)
 print([node.value for node in circularSLL])
