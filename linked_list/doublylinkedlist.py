ea38498d815182fa634f1252bd62a949c390055c
Combatd <combatdstar@gmail.com>
2022-08-01 02:45:06 -0700

    Create DoublyLinkedList class constructor

--- linked_list/doublylinkedlist.py
+++ linked_list/doublylinkedlist.py
@@ -8,3 +8,30 @@ class Node:
         self.next = None
         self.prev = None
 
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+
+    # Creation of Doubly Linked List
+    # Time Complexity; O(1) Space Complexity: O(1)
+    def createDLL(self, nodeValue):
+        node = Node(nodeValue) # O(1)
+        node.prev = None # O(1)
+        node.next = None # O(1)
+        self.head = node # O(1)
+        self.tail = node # O(1)
+        return 'The Doubly Linked List is created successfully'
+
+
+
+doublyLL = DoublyLinkedList()
+doublyLL.createDLL(5)
+
+print([node.value for node in doublyLL])
