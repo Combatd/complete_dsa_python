265cb6fdb89423dd1ef59df7d0fb4ef74e9fe386
Combatd <combatdstar@gmail.com>
2022-08-02 01:02:37 -0700

    Define CircularDoublyLinkedList#createCDLL

--- linked_list/circular_doubly_linked_list.py
+++ linked_list/circular_doubly_linked_list.py
@@ -1,10 +1,14 @@
+from platform import node
+from turtle import circle
+
+
 class Node:
     def __init__(self, value = None): # Time: O(1)
         self.value = value
         self.next = None
         self.prev = None
 
-class DoublyLinkedList:
+class CircularDoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
@@ -18,3 +22,18 @@ class DoublyLinkedList:
                 break
 
     # Creation of Circular Doubly Linked List
+    # Time Complexity: O(1) Space Complexity: O(1)
+    def createCDLL(self, nodeValue):
+        newNode = Node(nodeValue) # O(1)
+        self.head = newNode # O(1)
+        self.tail = newNode # O(1)
+        newNode.prev = newNode # O(1)
+        newNode.next = newNode # O(1)
+        return "The Circular Doubly Linked List is created successfully" # O(1)
+
+
+
+
+circularDLL = CircularDoublyLinkedList()
+print(circularDLL.createCDLL(5))
+print([node.value for node in circularDLL])
