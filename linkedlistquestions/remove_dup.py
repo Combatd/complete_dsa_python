30a666a605999f23d780b64880ebc99f125dc55d
Combatd <combatdstar@gmail.com>
2022-08-02 08:34:58 -0700

    Define better removeDups

--- linkedlistquestions/remove_dup.py
+++ linkedlistquestions/remove_dup.py
@@ -14,7 +14,25 @@ def removeDups(ll):
                 currentNode = currentNode.next
         return ll
 
+# Time Complexity: O(n^2) Space Complexity: O(1)
+def removeDups1(ll):
+    if ll.head is None: # O(1)
+        return
+    
+    currentNode = ll.head # O(1)
+    while currentNode: # O(n^2)
+        runner = currentNode
+        while runner.next: # O(n)
+            if runner.next.value == currentNode.value:
+                runner.next = runner.next.next
+            else:
+                runner = runner.next
+        currentNode = currentNode.next
+    return ll.head
+
+
 customLL = linked_list.LinkedList()
 customLL.generate(10, 0, 99)
-# print(customLL)
-removeDups(customLL)
+print(customLL)
+# removeDups(customLL)
+removeDups1(customLL)
