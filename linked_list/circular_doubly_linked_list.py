6041961c86f727f445692356fec0f5f6258e09e5
Combatd <combatdstar@gmail.com>
2022-08-02 00:58:25 -0700

    Create node class

---
+++ linked_list/circular_doubly_linked_list.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, value = None): # Time: O(1)
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+            if node == self.tail.next:
+                break
+
+    # Creation of Circular Doubly Linked List
