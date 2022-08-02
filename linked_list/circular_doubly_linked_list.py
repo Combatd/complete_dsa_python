af9b85f2bf41e2270bc72de8d43183631be0119c
Combatd <combatdstar@gmail.com>
2022-08-02 02:34:27 -0700

    Define #inserCDLL

--- linked_list/circular_doubly_linked_list.py
+++ linked_list/circular_doubly_linked_list.py
@@ -31,9 +31,41 @@ class CircularDoublyLinkedList:
         newNode.next = newNode # O(1)
         return "The Circular Doubly Linked List is created successfully" # O(1)
 
-
-
+    # Insertion method for Circular Doubly Linked List
+    # Time Complexity: O(n) Space Complexity: O(1)
+    def insertCDLL(self, value, location):
+        if self.head is None:
+            return "The Circular Doubly Linked List does not exist"
+        else:
+            newNode = Node(value)
+            if location == 0: # O(1)
+                newNode.next = self.head
+                newNode.prev = self.tail
+                self.head.prev = newNode
+                self.head = newNode
+                self.tail.next = newNode
+            elif location == -1: # O(1)
+                newNode.next = self.head
+                newNode.prev = self.tail
+                self.head.prev = newNode
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1: # O(n)
+                    tempNode = tempNode.next
+                    index += 1
+                newNode.next = tempNode.next
+                newNode.prev = tempNode
+                newNode.next.prev = newNode
+                tempNode.next = newNode
+        return "The node has been sucessfully inserted"
 
 circularDLL = CircularDoublyLinkedList()
 print(circularDLL.createCDLL(5))
+print(circularDLL.insertCDLL(0,0))
+print(circularDLL.insertCDLL(1, -1))
+print(circularDLL.insertCDLL(2, 2))
+
 print([node.value for node in circularDLL])
