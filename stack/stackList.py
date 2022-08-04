1cc26be926828aab3ba9ac025cef89ea1a4ae0ae
Combatd <combatdstar@gmail.com>
2022-08-04 00:18:15 -0700

    Define #push

--- stack/stackList.py
+++ stack/stackList.py
@@ -16,5 +16,17 @@ class Stack:
         else:
             return False
 
+    # push
+    # Time Complexity: Amortized O(1) - allocates a few times more memory on each call
+    # Space Complexity: O(1) - appending one element at a time
+    def push(self, value):
+        self.list.append(value)
+        return "The element has been successfully inserted"
+
+
 customStack = Stack()
 print(customStack.isEmpty())
+print(customStack.push(1))
+print(customStack.push(2))
+print(customStack.push(3))
+print(customStack)
