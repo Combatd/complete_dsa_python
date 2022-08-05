208ef63c6d3d412e493ca832ba20cd6c31284e8d
Combatd <combatdstar@gmail.com>
2022-08-05 10:55:54 -0700

    Create TreeNode class for Binary Tree

---
+++ trees/binaryTree.py
@@ -0,0 +1,12 @@
+class TreeNode:
+    # Time: O(1)
+    # Space: O(1)
+    def __init__(self, data):
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
+
+
+
+
+newBT = TreeNode("Drinks")
