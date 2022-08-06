ffcf0d5075a55dbfe7727dfc4913ef7a18301c87
Combatd <combatdstar@gmail.com>
2022-08-06 13:08:22 -0700

    Define #extractNode

--- trees/binaryHeap.py
+++ trees/binaryHeap.py
@@ -61,6 +61,62 @@ def insertNode(rootNode, nodeValue, heapType):
     heapifyTreeInsert(rootNode, rootNode.heapSize, heapType) # O(log n)
     return "The value has been successfully inserted" 
 
+# Time:
+# Space:
+def heapifyTreeExtract(rootNode, index, heapType):
+    leftIndex = index * 2
+    rightIndex = index * 2 + 1
+    swapChild = 0
+
+    if rootNode.heapSize < leftIndex: # if it has no children
+        return
+    elif rootNode.heapSize == leftIndex:
+        if heapType == "Min": # root is smaller than child
+            if rootNode.customList[index] > rootNode.customList[leftIndex]:
+                temp = rootNode.customList[index]
+                rootNode.customList[index] = rootNode.customList[leftIndex]
+                rootNode.customList[leftIndex] = temp
+            return
+        else:
+            if rootNode.customList[index] < rootNode.customList[leftIndex]:
+                temp = rootNode.customList[index]
+                rootNode.customList[index] = rootNode.customList[leftIndex]
+                rootNode.customList[leftIndex] = temp
+            return
+    
+    else:
+        if heapType == "Min":
+            if rootNode.customList[leftIndex] < rootNode.customList[rightIndex]:
+                swapChild = leftIndex
+            else:
+                swapChild = rightIndex
+            if rootNode.customList[index] > rootNode.customList[swapChild]:
+                temp = rootNode.customList[index]
+                rootNode.customList[index] = rootNode.customList[swapChild]
+                rootNode.customList[swapChild] = temp
+        else:
+            if rootNode.customList[leftIndex] > rootNode.customList[rightIndex]:
+                swapChild = leftIndex
+            else:
+                swapChild = rightIndex
+            if rootNode.customList[index] < rootNode.customList[swapChild]:
+                temp = rootNode.customList[index]
+                rootNode.customList[index] = rootNode.customList[swapChild]
+                rootNode.customList[swapChild] = temp
+    heapifyTreeExtract(rootNode, swapChild, heapType)
+
+# Time: O(log n)
+# Space: O(log n)
+def extractNode(rootNode, heapType):
+    if rootNode.heapSize == 0: # O(1)
+        return
+    else:
+        extractedNode = rootNode.customList[1]
+        rootNode.customList[1] = rootNode.customList[rootNode.heapSize]
+        rootNode.customList[rootNode.heapSize] = None
+        rootNode.heapSize -= 1
+        heapifyTreeExtract(rootNode, 1, heapType) # O(log n)
+        return extractedNode # O(1)
 
 
 
@@ -70,4 +126,5 @@ insertNode(newHeap, 4, "Max")
 insertNode(newHeap, 5, "Max")
 insertNode(newHeap, 2, "Max")
 insertNode(newHeap, 1, "Max")
+print(extractNode(newHeap, "Max"), " <-- extractNode")
 levelOrderTraversal(newHeap)
