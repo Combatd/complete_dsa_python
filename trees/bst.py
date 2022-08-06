bedb0ee87a800ab7676ad2fe3d1389fad71bf1fb
Combatd <combatdstar@gmail.com>
2022-08-06 07:11:34 -0700

    Define #insertNode

--- trees/bst.py
+++ trees/bst.py
@@ -1,4 +1,7 @@
 
+from logging import root
+
+
 class BSTNode:
     # Time: O(1)
     # Space: O(1)
@@ -7,4 +10,27 @@ class BSTNode:
         self.leftChild = None
         self.rightChild = None
 
+
+# Time: O(log n) - Recursion through binary tree will always take less than n nodes
+# Space: O(log n)
+def insertNode(rootNode, nodeValue):
+    if rootNode.data == None:
+        rootNode.data = nodeValue
+    elif nodeValue <= rootNode.data:
+        if rootNode.leftChild is None:
+            rootNode.leftChild = BSTNode(nodeValue)
+        else:
+            insertNode(rootNode.leftChild, nodeValue) # O(n / 2)
+        
+    else:
+        if rootNode.rightChild is None:
+            rootNode.rightChild = BSTNode(nodeValue) # O(n / 2)
+        else:
+            insertNode(rootNode.rightChild, nodeValue) # O(n / 2)
+    return "The node has been successfully inserteds"
+
 newBST = BSTNode(None)
+print(insertNode(newBST, 70))
+print(insertNode(newBST, 60))
+print(newBST.data)
+print(newBST.leftChild.data)
