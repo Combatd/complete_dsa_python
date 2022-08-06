411ae611d10a8dccfdc25d47b73f2058acb220d9
Combatd <combatdstar@gmail.com>
2022-08-06 04:51:30 -0700

    Define BinaryTree#insertNode

--- trees/binaryTreePL.py
+++ trees/binaryTreePL.py
@@ -6,5 +6,17 @@ class BinaryTree:
         self.lastUsedIndex = 0
         self.maxSize = size
 
+    # Time: O(1)
+    # Space: O(1)
+    def insertNode(self, value):
+        if self.lastUsedIndex + 1 == self.maxSize:
+            return "The Binary Tree is Full"
+        self.customList[self.lastUsedIndex + 1] = value
+        self.lastUsedIndex += 1
+        return "The value has been successfully inserted"
+
 
 newBT = BinaryTree(8)
+print(newBT.insertNode("Drinks"))
+newBT.insertNode("Hot")
+newBT.insertNode("Cold")
