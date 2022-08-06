084726f7f0424e7c3298cf2b9083c6c551713241
Combatd <combatdstar@gmail.com>
2022-08-06 13:44:24 -0700

    Define #insertString

--- trees/trie.py
+++ trees/trie.py
@@ -10,4 +10,20 @@ class Trie:
     def __init__(self):
         self.root = TrieNode()
 
+    # Time: O(m) - m is number of characters
+    # Space: O(m)
+    def insertString(self, word):
+        current = self.root
+        for i in word:
+            ch = i
+            node = current.children.get(ch)
+            if node == None:
+                node = TrieNode()
+                current.children.update({ch: node})
+            current = node
+        current.endOfString = True
+        print("Successfully inserted " + str(word))
+
 newTrie = Trie()
+newTrie.insertString("App")
+newTrie.insertString("Appl")
