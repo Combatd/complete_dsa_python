8063b11c6f52756020abfb70b165be49b5fe44d0
Combatd <combatdstar@gmail.com>
2022-08-07 12:41:24 -0700

    Define #topologicalSort

---
+++ graphs/topologicalSort.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self, numberofVertices):
+        self.graph = defaultdict(list)
+        self.numberofVertices = numberofVertices
+    
+    def addEdge(self, vertex, edge):
+        self.graph[vertex].append(edge)
+    
+    def topogologicalSortUtil(self, v, visited, stack):
+        visited.append(v)
+
+        for i in self.graph[v]: # O(E)
+            if i not in visited:
+                self.topogologicalSortUtil(i, visited, stack)
+        
+        stack.insert(0, v) # O(1)
+    
+    # Time O(V + E)
+    # Space: O(V + E)
+    def topologicalSort(self):
+
+        visited = [] # O(1)
+        stack = [] # O(1)
+
+        for k in list(self.graph): #O(E + V) edge + vertices
+            if k not in visited:
+                self.topogologicalSortUtil(k, visited, stack)
+        
+        print(stack)
+    
+    
+
+customGraph = Graph(8)
+customGraph.addEdge("A", "C")
+customGraph.addEdge("C", "E")
+customGraph.addEdge("E", "H")
+customGraph.addEdge("E", "F")
+customGraph.addEdge("F", "G")
+customGraph.addEdge("B", "D")
+customGraph.addEdge("B", "C")
+customGraph.addEdge("D", "F")
+
+customGraph.topologicalSort()
