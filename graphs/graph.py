a978e29e4d557828348defd30007cd248c7e248b
Combatd <combatdstar@gmail.com>
2022-08-07 10:56:26 -0700

    Define #add_vertex

--- graphs/graph.py
+++ graphs/graph.py
@@ -1,24 +1,43 @@
 
 
 
+# class Graph:
+#     def __init__(self, gdict = None):
+#         if gdict is None:
+#             gdict = {}
+#         self.gdict = gdict
+
+#     def addEdge(self, vertex, edge):
+#         self.gdict[vertex].append(edge)
+
+
+# customDict = { "a" : ["b","c"],
+#             "b" : ["a", "d", "e"],
+#             "c" : ["a", "e"],
+#             "d" : ["b", "e", "f"],
+#             "e" : ["d", "f"],
+#             "f" : ["d", "e"]
+#                }
+
+# graph = Graph(customDict)
+# graph.addEdge("e", "c")
+# print(graph.gdict["e"])
+
 class Graph:
-    def __init__(self, gdict = None):
-        if gdict is None:
-            gdict = {}
-        self.gdict = gdict
+    def __init__(self):
+        self.adjacency_list = {}
 
-    def addEdge(self, vertex, edge):
-        self.gdict[vertex].append(edge)
+    def add_vertex(self, vertex):
+        if vertex not in self.adjacency_list.keys():
+            self.adjacency_list[vertex] = []
+            return True
+        return False
+    
+    def print_graph(self):
+        for vertex in self.adjacency_list:
+            print(vertex, ":", self.adjacency_list[vertex])
 
 
-customDict = { "a" : ["b","c"],
-            "b" : ["a", "d", "e"],
-            "c" : ["a", "e"],
-            "d" : ["b", "e", "f"],
-            "e" : ["d", "f"],
-            "f" : ["d", "e"]
-               }
-
-graph = Graph(customDict)
-graph.addEdge("e", "c")
-print(graph.gdict["e"])
+custom_graph = Graph()
+custom_graph.add_vertex("A")
+custom_graph.print_graph()
