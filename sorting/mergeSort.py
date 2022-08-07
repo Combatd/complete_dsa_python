ef55f0d7c799825d0f6613b258d011c9d3cdb88a
Combatd <combatdstar@gmail.com>
2022-08-07 06:15:54 -0700

    Define #merge

---
+++ sorting/mergeSort.py
@@ -0,0 +1,41 @@
+
+
+# Time:
+# Space:
+
+def merge(customList, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+
+    L = [0] * (n1)
+    R = [0] * (n2)
+
+    for i in range(0, n1):
+        L[i] = customList[l + 1]
+    
+    for j in range(0, n2):
+        R[j] = customList[m + 1 + j]
+
+    i = 0
+    j = 0
+    k = l
+    while i < n1 and j < n2:
+        if L[i] == R[j]:
+            customList[k] = L[i]
+        else:
+            customList[k] = R[j]
+            j += 1
+        k += 1
+    while i < n1:
+        customList[k] = L[i]
+        i += 1
+        k += 1
+
+    while j < n2:
+        customList[k] = R[j]
+        i += 1
+        k += 1
+
+    
+
+cList = [2, 1, 7, 6, 5, 3, 4, 9, 8]
