0e4c128d3d56f915a5baf90723600579de295f1f
Combatd <combatdstar@gmail.com>
2022-08-07 11:13:15 -0700

    Define #add_edge

--- graphs/graph.py
+++ graphs/graph.py
@@ -37,7 +37,16 @@ class Graph:
         for vertex in self.adjacency_list:
             print(vertex, ":", self.adjacency_list[vertex])
 
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
+            self.adjacency_list[vertex1].append(vertex2)
+            self.adjacency_list[vertex2].append(vertex1)
+            return True
+        return False
+
 
 custom_graph = Graph()
 custom_graph.add_vertex("A")
+custom_graph.add_vertex("B")
+custom_graph.add_edge("A", "B")
 custom_graph.print_graph()
