f3900a5e98355572e3874abf262a5aa5a34949a1
Combatd <combatdstar@gmail.com>
2022-08-07 13:48:22 -0700

    Create Graph class

---
+++ graphs/dijkstra.py
@@ -0,0 +1,14 @@
+
+
+class Graph:
+    def __init__(self):
+        self.nodes = set()
+        self.edges = defaultdict(list)
+        self.distances = {}
+    
+    def addNode(self,value):
+        self.nodes.add(value)
+    
+    def addEdge(self, fromNode, toNode, distance):
+        self.edges[fromNode].append(toNode)
+        self.distances[(fromNode, toNode)] = distance
