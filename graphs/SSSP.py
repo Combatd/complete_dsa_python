41302947d3f3f8d4ade4c53f81e4044b30b1061c
Combatd <combatdstar@gmail.com>
2022-08-07 13:29:36 -0700

    Define #bfs

---
+++ graphs/SSSP.py
@@ -0,0 +1,42 @@
+# BFS
+
+# enqueue any starting vertex
+# while queue is not empty
+    # p = dequeue()
+    # if p is unvisited
+        # mark it visited
+        # enqueue all adjacent unvisited vertices of p
+        # update parent of adjacent vertices to currentVertex
+
+class Graph:
+    def __init__(self, gdict = None):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+    
+    # Time: Q(E)
+    # Space: Q(E)
+    def bfs(self, start, end):
+        queue = []
+        queue.append([start])
+        while queue:
+            path = queue.pop(0)
+            node = path[-1]
+            if node == end:
+                return path
+            for adjacent in self.gdict.get(node, []):
+                new_path = list(path)
+                new_path.append(adjacent)
+                queue.append(new_path)
+
+
+customDict = { "a" : ["b", "c"],
+               "b" : ["d", "g"],
+               "c" : ["d", "e"],
+               "d" : ["f"],
+               "e" : ["f"],
+               "g" : ["f"]
+            }
+
+g = Graph(customDict)
+print(g.bfs("a", "e"))
