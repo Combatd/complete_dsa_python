94c69e650c47322e42a611453601cd0bcfaaa54a
Combatd <combatdstar@gmail.com>
2022-08-07 15:53:27 -0700

    Create DisjointSet class

---
+++ graphs/disjointSet.py
@@ -0,0 +1,38 @@
+
+
+# Disjoint Set
+# Data structure that keeps track of set of elements which are partitioned into a
+# number of disjoint and non-overlapping sets and each sets have representative which helps in identifying
+# that sets.
+
+class DisjointSet:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.parent = {}
+        for v in vertices:
+            self.parent[v] = v
+        self.rank = dict.fromkeys(vertices, 0)
+    
+    def find(self, item):
+        if self.parent[item] == item:
+            return item
+        else:
+            return self.find(self.parent[item])
+    
+    def union(self, x, y):
+        xroot = self.find(x)
+        yroot = self.find(y)
+        if self.rank[xroot] < self.rank[yroot]:
+            self.parent[xroot] = yroot
+        elif self.rank[xroot] > self.rank[yroot]:
+            self.parent[yroot] = xroot
+        else:
+            self.parent[yroot] = xroot
+            self.rank[xroot] += 1
+
+# vertices = ["A", "B", "C", "D", "E"]
+
+# ds = DisjointSet(vertices)
+# ds.union("A", "B")
+# ds.union("A", "C")
+# print(ds.find("A"))
