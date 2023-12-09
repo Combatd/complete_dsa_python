a51a35fe7c23ac85781794a878381842fafd6f31
Combatd <combatdstar@gmail.com>
2023-12-09 02:25:16 -0800

    Invoke Tuple methods

---
+++ tuples/tuples.py
@@ -0,0 +1,13 @@
+myTuple = (1,4,3,2,5)
+myTuple1 = (1,2,6,9,8,7)
+
+print(myTuple * 4)
+print(4 in myTuple)
+
+print(myTuple.count(2))
+print(myTuple.index(4))
+
+print(len(myTuple))
+print(max(myTuple))
+print(min(myTuple1))
+print(tuple([1, 2, 3, 4]))
