763a6008fa2ad33655d1f78d0eb7ed7f67fbfeb3
Combatd <combatdstar@gmail.com>
2023-12-09 01:51:05 -0800

    Traverse tuple

---
+++ tuples/traverseTuple.py
@@ -0,0 +1,20 @@
+newTuple = (
+  'a',
+  'b',
+  'c',
+  'd',
+  'e'
+)
+
+for i in range(len(newTuple)):
+  print(newTuple[i])
+
+
+"""  
+Time Complexity: O(n)
+We have to visit each and every element of the tuple, n number of elements
+
+Space Complexity: O(1)
+Additional memory for the tuple is not required when traversing its elements
+
+"""
