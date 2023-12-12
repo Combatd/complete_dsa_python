5c69e47acfacb7e84d463a0e1c8ad17544c9966c
Combatd <combatdstar@gmail.com>
2023-12-11 23:59:41 -0800

    Create StarCookie class

---
+++ oop/StarCookie.py
@@ -0,0 +1,15 @@
+class StarCookie:
+  def __init__(self, color, weight) -> None:
+    self.color = color
+    self.weight = weight
+
+
+  
+star_cookie1 = StarCookie('red', 15)
+print(star_cookie1.weight)
+print(star_cookie1.color)
+
+star_cookie2 = StarCookie('blue', 16)
+
+print(star_cookie2.weight)
+print(star_cookie2.color)
