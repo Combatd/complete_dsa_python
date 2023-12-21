22d0a50be25f4538214654cbe5dc26208f88e32b
Combatd <combatdstar@gmail.com>
2023-12-21 15:51:17 -0800

    Define LinkedList#prepend

--- linked_list/SLL.py
+++ linked_list/SLL.py
@@ -30,9 +30,22 @@ class LinkedList:
       self.tail.next = new_node
       self.tail = new_node
     self.length += 1
+
+  def prepend(self, value):
+    new_node = Node(value)
+    if self.head is None:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      new_node.next = self.head
+      self.head = new_node
+    self.length += 1
     
 
 new_linked_list = LinkedList()
 new_linked_list.append(10)
 new_linked_list.append(20)
+new_linked_list.append(30)
+print(new_linked_list)
+new_linked_list.prepend(40)
 print(new_linked_list)
