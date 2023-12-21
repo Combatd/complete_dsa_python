ec6241e701f09ac87a8e4e7150c078e22e8fae09
Combatd <combatdstar@gmail.com>
2023-12-21 15:22:09 -0800

    Override __str__ in LinkedList

--- linked_list/SLL.py
+++ linked_list/SLL.py
@@ -11,6 +11,16 @@ class LinkedList:
     self.tail = None
     self.length = 0
 
+  def __str__(self):
+    temp_node = self.head
+    result = ''
+    while temp_node is not None:
+      result += str(temp_node.value)
+      if temp_node.next is not None:
+        result += ' -> '
+      temp_node = temp_node.next
+    return result
+
   def append(self, value):
     new_node = Node(value)
     if self.head is None:
@@ -20,6 +30,7 @@ class LinkedList:
       self.tail.next = new_node
       self.tail = new_node
     self.length += 1
+    
 
 new_linked_list = LinkedList()
 new_linked_list.append(10)
