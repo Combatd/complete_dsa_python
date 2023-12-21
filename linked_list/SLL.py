a3b842d6ccd8bd1d86bfb742153324962854ff1a
Combatd <combatdstar@gmail.com>
2023-12-21 15:07:17 -0800

    Define LinkedList#append

--- linked_list/SLL.py
+++ linked_list/SLL.py
@@ -3,26 +3,25 @@ class Node:
     self.value = value
     self.next = None
 
-new_node = Node(10)
-print(new_node)
-# Time and Space O(1)
+
 
 class LinkedList:
-  def __init__(self, value):
+  def __init__(self):
+    self.head = None
+    self.tail = None
+    self.length = 0
+
+  def append(self, value):
     new_node = Node(value)
-    self.head = new_node
-    self.tail = new_node
-    self.length = 1
+    if self.head is None:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      self.tail.next = new_node
+      self.tail = new_node
+    self.length += 1
 
-new_linked_list = LinkedList(10)
+new_linked_list = LinkedList()
+new_linked_list.append(10)
+new_linked_list.append(20)
 print(new_linked_list)
-print(new_linked_list.head.value)
-
-""" 
-Linked List Initialization with one node
-Time Complexity: O(1)
-Only one node needs to be initialized as the Linked List object is initialized.
-
-Space Complexity: O(1)
-Only one Node is created to be in one new LinkedList so it is a constant space usage.
-"""
