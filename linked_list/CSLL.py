976a819353ba19146604ab55b688a53ea1ec2624
Combatd <combatdstar@gmail.com>
2023-12-25 04:42:23 -0800

    Define CSLinkedList#append

--- linked_list/CSLL.py
+++ linked_list/CSLL.py
@@ -1,7 +1,7 @@
 class Node:
   def __init__(self, value):
-    this.value = value
-    this.next = None
+    self.value = value
+    self.next = None
 
 class CSLinkedList:
   def __init__(self):
@@ -9,5 +9,21 @@ class CSLinkedList:
     self.tail = None
     self.length = 0
 
+  def append(self, value):
+    new_node = Node(value)
+    if self.length == 0:
+      self.head = new_node
+      self.tail = new_node
+      new_node.next = new_node
+    else:
+      self.tail.next = new_node
+      new_node.next = self.head
+      self.tail = new_node
+    self.length += 1
+
 csLinkedList = CSLinkedList()
-print(csLinkedLIst.head)
+
+print(csLinkedList.append(10))
+print(csLinkedList.append(20))
+print(csLinkedList.head.value)
+print(csLinkedList.tail.value)
