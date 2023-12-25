550ffd00026515b9d168ed985bf70f88ec68e07a
Combatd <combatdstar@gmail.com>
2023-12-25 04:31:18 -0800

    Create class CSLinkedList

---
+++ linked_list/CSLL.py
@@ -0,0 +1,13 @@
+class Node:
+  def __init__(self, value):
+    this.value = value
+    this.next = None
+
+class CSLinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+    self.length = 0
+
+csLinkedList = CSLinkedList()
+print(csLinkedLIst.head)
