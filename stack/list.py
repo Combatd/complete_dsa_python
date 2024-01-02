d21630316bce476f0b0a88cc8af979b72369cc4a
Combatd <combatdstar@gmail.com>
2024-01-01 17:53:30 -0800

    Define #pop

--- stack/list.py
+++ stack/list.py
@@ -14,12 +14,22 @@ class Stack:
     else:
       return False
 
+  # push
   def push(self, value):
     self.list.append(value)
     return "The element has been successfully inserted"
+
+  # pop
+  def pop(self):
+    if self.isEmpty():
+      return 'There is not any element in the list'
+    else:
+      return self.list.pop()
 
 customStack = Stack()
 print(customStack.isEmpty())
 customStack.push(1)
 customStack.push(2)
 customStack.push(3)
+
+print(customStack.pop())
