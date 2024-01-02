ae463b8d3df83b193ea4dccac7c7aa4742898bb8
Combatd <combatdstar@gmail.com>
2024-01-01 22:49:48 -0800

    Define LLstack #pop

--- stack/LLstack.py
+++ stack/LLstack.py
@@ -16,7 +16,6 @@ class LinkedList:
 class Stack:
   def __init__(self):
     self.LinkedList = LinkedList()
-    self.length = 0
 
   def __str__(self):
     values = [str(x.value) for x in self.LinkedList]
@@ -37,7 +36,12 @@ class Stack:
 
   # pop
   def pop(self):
-    pass
+    if self.isEmpty():
+      return 'There is not any element in the stack'
+    else:
+      nodeValue = self.LinkedList.head.value
+      self.LinkedList.head = self.LinkedList.head.next
+      return nodeValue
 
   # peek
   def peek(self):
@@ -55,4 +59,8 @@ print(customStack.isEmpty())
 customStack.push(1)
 customStack.push(2)
 customStack.push(3)
+print(customStack)
+print("------")
+print(customStack.pop())
+print('------')
 print(customStack)
