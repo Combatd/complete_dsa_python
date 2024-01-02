f10f98a11885856b53c25d4d52fec162f9a24b9f
Combatd <combatdstar@gmail.com>
2024-01-01 17:51:54 -0800

    Define #push:

--- stack/list.py
+++ stack/list.py
@@ -1,8 +1,25 @@
 class Stack:
   def __init__(self):
-    this.list = []
+    self.list = []
 
-    def __str__(self):
-      values = self.list.reverse()
-      values = [str(x) for x in self.list]
-      return '\n'.join(values)
+  def __str__(self):
+    values = self.list.reverse()
+    values = [str(x) for x in self.list]
+    return '\n'.join(values)
+
+  # isEmpty
+  def isEmpty(self):
+    if self.list == []:
+      return True
+    else:
+      return False
+
+  def push(self, value):
+    self.list.append(value)
+    return "The element has been successfully inserted"
+
+customStack = Stack()
+print(customStack.isEmpty())
+customStack.push(1)
+customStack.push(2)
+customStack.push(3)
